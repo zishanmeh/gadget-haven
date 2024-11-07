bc4ad7ae88fbb2942c88bd00b32ee0f5a3203932
zishanmeh <zishanmehedihasan2@gmail.com>
2024-11-07 05:09:47 +0000

    Added some functionality to purchase cart

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ export const wishListContext = createContext([]);
 export const handleCartContext = createContext(() => {});
 export const handleWishListContext = createContext(() => {});
 export const handleRemoveCartContext = createContext(() => {});
+export const handleSortBypriceContext = createContext(() => {});
+export const handlePurchaseContext = createContext(() => {});
+export const totalPriceContext = createContext();
+export const previousTotalPriceContext = createContext();
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -41,6 +45,27 @@ function App() {
     }
   };
 
+  const handleSortByprice = () => {
+    const sortedCart = [...cart].sort((a, b) => b.price - a.price);
+    setCart(sortedCart);
+  };
+
+  let totalPrice = 0;
+  for (const c of cart) {
+    totalPrice = totalPrice + c.price;
+  }
+
+  const [previousTotalPrice, setPreviousTotalPrice] = useState(0);
+  const handlePurchase = () => {
+    if (cart.length > 0) {
+      setPreviousTotalPrice(totalPrice);
+      document.getElementById("my_modal_5").showModal();
+      setCart([]);
+    } else {
+      toast.error("No item on cart 😭");
+    }
+  };
+
   return (
     <div className="">
       <cartContext.Provider value={cart}>
@@ -48,9 +73,19 @@ function App() {
           <handleCartContext.Provider value={handleAddToCart}>
             <handleWishListContext.Provider value={handleWishList}>
               <handleRemoveCartContext.Provider value={handleRemoveCart}>
-                <Navbar></Navbar>
-                <Outlet></Outlet>,
-                <ToastContainer />
+                <handleSortBypriceContext.Provider value={handleSortByprice}>
+                  <handlePurchaseContext.Provider value={handlePurchase}>
+                    <totalPriceContext.Provider value={totalPrice}>
+                      <previousTotalPriceContext.Provider
+                        value={previousTotalPrice}
+                      >
+                        <Navbar></Navbar>
+                        <Outlet></Outlet>,
+                        <ToastContainer />
+                      </previousTotalPriceContext.Provider>
+                    </totalPriceContext.Provider>
+                  </handlePurchaseContext.Provider>
+                </handleSortBypriceContext.Provider>
               </handleRemoveCartContext.Provider>
             </handleWishListContext.Provider>
           </handleCartContext.Provider>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,24 +1,43 @@
 import React, { useContext } from "react";
-import { cartContext, handleRemoveCartContext } from "../../App";
+import {
+  cartContext,
+  handleRemoveCartContext,
+  handleSortBypriceContext,
+  handlePurchaseContext,
+  totalPriceContext,
+  previousTotalPriceContext,
+} from "../../App";
 import { IoIosCloseCircleOutline } from "react-icons/io";
+import Image from "../../assets/Group.png";
 
 const Cart = () => {
-  const cart = useContext(cartContext);
+  let cart = useContext(cartContext);
   const handleRemoveCart = useContext(handleRemoveCartContext);
-  let totalPrice = 0;
-  for (const c of cart) {
-    totalPrice = totalPrice + c.price;
-  }
+  const handleSortByprice = useContext(handleSortBypriceContext);
+  const handlePurchase = useContext(handlePurchaseContext);
+  const totalPrice = useContext(totalPriceContext);
+  const previousTotalPrice = useContext(previousTotalPriceContext);
+  //   let totalPrice = 0;
+  //   for (const c of cart) {
+  //     totalPrice = totalPrice + c.price;
+  //   }
+
   return (
     <div className="w-11/12 mx-auto my-10">
       <div className="flex justify-between items-center">
         <h1 className="font-bold text-xl">Cart</h1>
         <div className="flex justify-end items-center gap-4">
           <h4 className="font-bold text-xl">Total Cost : {totalPrice}</h4>
-          <button className="btn bg-transparent border border-primaryColor px-5 py-3 rounded-full text-primaryColor hover:bg-primaryColor hover:text-white">
+          <button
+            onClick={handleSortByprice}
+            className="btn bg-transparent border border-primaryColor px-5 py-3 rounded-full text-primaryColor hover:bg-primaryColor hover:text-white"
+          >
             Sort by price
           </button>
-          <button className="btn hover:bg-white hover:text-primaryColor bg-primaryColor px-5 py-3 rounded-full border border-primaryColor text-white">
+          <button
+            onClick={handlePurchase}
+            className="btn hover:bg-white hover:text-primaryColor bg-primaryColor px-5 py-3 rounded-full border border-primaryColor text-white"
+          >
             Purchase
           </button>
         </div>
@@ -52,6 +71,24 @@ const Cart = () => {
             ))
           : "No Data Found"}
       </div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box flex justify-center flex-col gap-4 items-center">
+          <img src={Image} alt="" />
+          <h1 className="font-bold text-2xl">Payment successfull</h1>
+          <p className="text-gray-600">Thanks for Purchasing...</p>
+          <h3 className="font-bold text-gray-600">
+            Total Price: ${previousTotalPrice}
+          </h3>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
